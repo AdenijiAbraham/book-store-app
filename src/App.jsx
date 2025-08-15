3eb466ee2be69acd464e62a9c2294ef3e1bb894d
Abraham <114365667+AdenijiAbraham@users.noreply.github.com>
2025-08-15 16:19:19 +0100

    Footer updated in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Outlet } from 'react-router-dom' 
 import Navbar from './components/Navbar';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 import { AuthProvider } from './context/Auth.Context';
 
 
